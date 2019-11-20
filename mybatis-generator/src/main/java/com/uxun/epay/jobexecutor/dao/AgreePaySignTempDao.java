09e11b2acdc21f5a953bbf9d771bfed438cd08ad
jinm <jinm@uxunchina.com>
2019-11-20 17:51:20 +0800

    create project: mybatis-generator

---
+++ mybatis-generator/src/main/java/com/uxun/epay/jobexecutor/dao/AgreePaySignTempDao.java
@@ -0,0 +1,17 @@
+package com.uxun.epay.jobexecutor.dao;
+
+import com.uxun.epay.jobexecutor.entity.AgreePaySignTemp;
+
+public interface AgreePaySignTempDao {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(AgreePaySignTemp record);
+
+    int insertSelective(AgreePaySignTemp record);
+
+    AgreePaySignTemp selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(AgreePaySignTemp record);
+
+    int updateByPrimaryKey(AgreePaySignTemp record);
+}
